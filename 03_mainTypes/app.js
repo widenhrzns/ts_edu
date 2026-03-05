a08e5629c22f1e56613ba039192026e97d8fc31d
widenhrzns <widenhrzns@gmail.com>
2026-03-05 18:23:21 +0300

    Enums

--- 03_mainTypes/app.js
+++ 03_mainTypes/app.js
@@ -1,7 +1,36 @@
 "use strict";
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонен
 Object.defineProperty(exports, "__esModule", { value: true });
-const skills = [1, "Dev"];
-// skills[0] = 2; теперь нельзя поменять, только чтение
-const skill = ["Dev", "DevOps"];
-// skill.push('test') не можем :)))
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode["IN_PROCESS"] = "p";
+    StatusCode["FAILED"] = "f";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: "Платеж успешен",
+    statusCode: StatusCode.SUCCESS,
+};
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) { }
+action(StatusCode.SUCCESS);
+action(1); // вот это уже плохо, тут вниматнльно
+// action(3);
+// action('p') - а вот такое уже не пойдет, особоенность enum
+//////////////////
+// function compute() {
+//   return 3;
+// }
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 1] = "ADMIN";
+    Roles[Roles["USER"] = 2] = "USER";
+})(Roles || (Roles = {}));
+const res2 = Roles.ADMIN;
+/////////////////
+// function test(x: { ADMIN: number }) {}
+// test(Roles); // допустимо, но ирл не используют (enum ведет себя как объект )
 //# sourceMappingURL=app.js.map
--- 03_mainTypes/app.ts
+++ 03_mainTypes/app.ts
@@ -1,7 +1,42 @@
-const skills: readonly [number, string] = [1, "Dev"];
+// 1 - успех
+// 'p' - в процессе
+// 'f' - отклонен
 
-// skills[0] = 2; теперь нельзя поменять, только чтение
+enum StatusCode {
+  SUCCESS = 1,
+  IN_PROCESS = "p",
+  FAILED = "f",
+}
 
-const skill: ReadonlyArray<string> = ["Dev", "DevOps"];
+const res = {
+  message: "Платеж успешен",
+  statusCode: StatusCode.SUCCESS,
+};
 
-// skill.push('test') не можем :)))
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+
+function action(status: StatusCode) {}
+action(StatusCode.SUCCESS);
+action(1); // вот это уже плохо, тут вниматнльно
+// action(3);
+// action('p') - а вот такое уже не пойдет, особоенность enum
+
+//////////////////
+
+// function compute() {
+//   return 3;
+// }
+
+const enum Roles {
+  ADMIN = 1,
+  USER = 2, //compute(),
+}
+
+const res2 = Roles.ADMIN;
+
+/////////////////
+
+// function test(x: { ADMIN: number }) {}
+
+// test(Roles); // допустимо, но ирл не используют (enum ведет себя как объект )
