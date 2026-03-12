12a2d9763c9102d495bb7ac0770aa4062b20a8c6
widenhrzns <widenhrzns@gmail.com>
2026-03-12 13:40:29 +0300

    Null

--- 04_advancedTypes/app.ts
+++ 04_advancedTypes/app.ts
@@ -1,39 +1,26 @@
-function generateError(message: string): never {
-  throw new Error(message);
+const n: null = null;
+// const n1: null = undefined;
+const n2: any = null;
+// const n3: number = null
+// const n4: boolean = null
+// const n5: string = null
+// const n6: undefined = null
+
+interface User {
+  name: string;
 }
 
-function dumpError(): never {
-  // return ''
-  while (true) {}
-}
-
-function rec(): never {
-  return rec();
-}
-
-// const a: never = 1
-
-type paymentAction = "refund" | "checkout"; //| "reject";
-
-function processAction(action: paymentAction) {
-  switch (action) {
-    case "refund":
-      // ...
-      break;
-    case "checkout":
-      // ...
-      break;
-    default:
-      const _: never = action;
-      throw new Error("Нет такого Action");
+function getUser() {
+  if (Math.random() > 0.5) {
+    return null;
+  } else {
+    return {
+      name: "Vasya",
+    } as User;
   }
 }
-/** */
-function isString(x: string | number): boolean {
-  if (typeof x === "string") {
-    return true;
-  } else if (typeof x === "number") {
-    return false;
-  }
-  generateError("sdkfls");
+
+const user = getUser();
+if (user) {
+  const name = user.name;
 }
